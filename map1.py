456a81cee17054b5e48ff167442ad413ce1eb833
hugodmorais <hugodanielmorais@gmail.com>
2020-05-12 23:18:03 +0100

    multiple marker

--- map1.py
+++ map1.py
@@ -1,10 +1,16 @@
 import folium
-map = folium.Map(location=[38.759, -9.115], zoom_start=12, tiles="Stamen Terrain")
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])ist(data["LO
+lon = lN"])
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.750, -9.119],[38.740, -9.119],[38.737, -9.121],[38.747, -9.130],[38.755, -9.125],[38.751, -9.121]]:
-  fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat, lon):
+  fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
