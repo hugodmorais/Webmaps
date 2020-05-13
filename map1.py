c8d99fa2a4628fba45e16420ef0a68d557c4ac2b
hugodmorais <hugodanielmorais@gmail.com>
2020-05-13 09:21:41 +0100

    Add color to marker points

--- map1.py
+++ map1.py
@@ -6,12 +6,20 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+  if elevation < 1000:
+    return 'green'
+  elif 1000 <= elevation < 3000:
+    return 'orange'
+  else:
+    return 'red'
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-  fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
+  fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
 map.add_child(fg)
 
 map.save("Map1.html")
