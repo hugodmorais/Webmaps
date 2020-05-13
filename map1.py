ddfd86d442ef66504aeea52aea974f35913d8b17
hugodmorais <hugodanielmorais@gmail.com>
2020-05-13 11:00:22 +0100

    CircleMarker

--- map1.py
+++ map1.py
@@ -19,7 +19,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-  fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color=color_producer(el))))
+  fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m", 
+  fill_color=color_producer(el), color = 'grey', fill=True, fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("Map1.html")
