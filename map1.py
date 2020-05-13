f5aecc919d5637d66e52ab83a94d6985c974649f
hugodmorais <hugodanielmorais@gmail.com>
2020-05-13 09:12:40 +0100

    Edit marker, add elev point

--- map1.py
+++ map1.py
@@ -2,15 +2,16 @@ import folium
 import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
-lat = list(data["LAT"])ist(data["LO
-lon = lN"])
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-  fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+  fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
