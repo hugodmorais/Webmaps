5666298b5efae1ce1c5c8c607bacfd1d27c6ae56
hugodmorais <hugodanielmorais@gmail.com>
2020-05-14 09:29:21 +0100

    Add 'Volcanoes and Population' features, add GeoJson and finish project

--- map1.py
+++ map1.py
@@ -16,11 +16,18 @@ def color_producer(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-  fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m", 
+  fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m", 
   fill_color=color_producer(el), color = 'grey', fill=True, fill_opacity=0.7))
-map.add_child(fg)
 
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
